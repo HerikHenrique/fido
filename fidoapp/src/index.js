d07a397b346d62f96eac19120585a166ca73278a
HerikHenrique <74871202+HerikHenrique@users.noreply.github.com>
2022-02-23 23:50:38 -0300

    Create Center on Page

--- fidoapp/src/index.js
+++ fidoapp/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MuiDrawer from './MuiDrawer'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MuiDrawer/>
+    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
